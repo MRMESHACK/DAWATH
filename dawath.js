7e71ac3312e77daf6a17b547e98b3f6e1b00fe9e
MRMESHACK <mrmeshack23@gmail.com>
2021-09-16 17:18:49 +0530

    first commit

---
+++ dawath.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  
+
+});
